6dc797afa1d485c4375ceb186e0bbb4be38765b1
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-07-14 23:14:23 +0000

    Pedro creates first model with three table columns

---
+++ ecommerce-masonry/ecommerce-masonry/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ecommerce_masonry.Models
+{
+    public class Category // Inside category are the columns we want! These are properties of this class.
+    {
+        [Key] // Once we add this annotation it tells entity framework that this column needs to be an id column and pkey for our table
+        public int ID { get; set; }
+        public string CategoryName { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
