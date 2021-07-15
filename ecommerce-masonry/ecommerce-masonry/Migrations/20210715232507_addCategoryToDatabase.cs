f7d1c9d4ac7e9a05c99b63cd654a174f9ed5b5ec
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-07-15 23:41:54 +0000

    Pedro adds a migration then pushes it to the database. Steps are -> Nuget Package Manager -> Microsoft.EntityFrameworkCore.Tools v 5.0.8 -> Package Manager Console -> add-migration addCategoryToDatabase -> Boom, our migration has been added. Entity framework code is great. Table named Category has been created, with three columns, Id, Name and DisplayOrder. Model was converted into migration, then pushed to database. -> Package Manager Console -> update-database

---
+++ ecommerce-masonry/ecommerce-masonry/Migrations/20210715232507_addCategoryToDatabase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ecommerce_masonry.Migrations
+{
+    public partial class addCategoryToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DisplayOrder = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Category");
+        }
+    }
+}
