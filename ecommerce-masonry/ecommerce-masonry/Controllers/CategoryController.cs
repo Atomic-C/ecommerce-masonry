4b066919bf14366ec1911706f0f25e359a5f1b57
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-07-16 23:32:00 +0000

    Pedro manually adds three categories on ECM database using Microsoft SQL, for testing purposes. To display these categories on the web site, we need to extract them from the database using entityframework, using application dbcontext. We add a private readonly property, and we populate it using a constructor, using dependency injection.

--- ecommerce-masonry/ecommerce-masonry/Controllers/CategoryController.cs
+++ ecommerce-masonry/ecommerce-masonry/Controllers/CategoryController.cs
@@ -1,16 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using ecommerce_masonry.Data;
+using ecommerce_masonry.Models;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace ecommerce_masonry.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> objectList = _db.Category;
+            return View(objectList);
         }
     }
 }
