564282f9d1b02402f341c70ebd844e68d6aa8306
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-07-21 23:38:05 +0000

    Pedro comments Catrgory model class, and fixes wrong class name

--- ecommerce-masonry/ecommerce-masonry/Models/Category.cs
+++ ecommerce-masonry/ecommerce-masonry/Models/Category.cs
@@ -2,11 +2,12 @@
 
 namespace ecommerce_masonry.Models
 {
-    public class ApplicationType // Inside category are the columns we want! These are properties of this class.
+    public class Category // This class represents our table. Model represents a table we want in our database.
     {
-        [Key] // Once we add this annotation it tells entity framework that this column needs to be an id column and pkey for our table
-        public int ID { get; set; }
-        public string CategoryName { get; set; }
+        [Key] // This data annotation tells entity framework that this column needs to be an identity column and a pkey for our table
+        public int ID { get; set; } // This is a class property and represents a column
+        public string CategoryName { get; set; } // This is a class property and represents a column
+        public int DisplayOrder { get; set; } // This is a class property and represents a column
 
     }
 }
