dfa487cc08b2e4dc77733ecfc4443d4d99ec2090
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-07-21 23:10:36 +0000

    Pedro creates first model, all commented. So a model is a class that represents the table we want in our database, and the columns are class properties.

---
+++ ecommerce-masonry/ecommerce-masonry/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ecommerce_masonry.Models
+{
+    public class Category // This class IS the table. This is the model that represents the table we want in our database.
+    {
+        [Key] // This data anotation is used to tell entity framework that this column is an identity column and a pkey.
+        public int Id { get; set; } // This property is a table column
+        public int Name { get; set; } // This property is a table column
+        public int DisplayOrder { get; set; } // This property is a table column
+    }
+}
