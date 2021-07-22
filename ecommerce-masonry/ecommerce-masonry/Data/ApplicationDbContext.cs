631d1b68c66b36a8f3ed7e9e185d49a442f701eb
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-07-22 02:01:53 +0000

    Pedro comments ApplicationDbContext

--- ecommerce-masonry/ecommerce-masonry/Data/ApplicationDbContext.cs
+++ ecommerce-masonry/ecommerce-masonry/Data/ApplicationDbContext.cs
@@ -3,7 +3,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ecommerce_masonry.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : DbContext // Inherit attributes and methods from DbContext class
+    // DbContext only exists with sql server and entity framework packages installed
+
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
