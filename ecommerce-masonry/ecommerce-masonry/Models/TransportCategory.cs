2aef87a0ab11a5cdc54e307c4d91aff8d45de243
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-08-02 21:51:19 +0000

    Pedro comments some lines for better understanding, adds TransportCategoryController

--- ecommerce-masonry/ecommerce-masonry/Models/TransportCategory.cs
+++ ecommerce-masonry/ecommerce-masonry/Models/TransportCategory.cs
@@ -5,8 +5,8 @@ namespace ecommerce_masonry.Models
     public class TransportCategory
     {
         [Key]
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public int Id { get; set; } // This is a class property and represents a column
+        public string Name { get; set; } // This is a class property and represents a column
 
     }
 }
