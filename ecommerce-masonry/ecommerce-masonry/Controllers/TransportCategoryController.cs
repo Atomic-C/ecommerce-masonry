689e3017c585271cb640ca74f8a2eefe09d90619
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-08-06 23:27:17 +0000

    Atributes removed. Apparently without atributes we can insert null values inside the columns and this breaks the database.

--- ecommerce-masonry/ecommerce-masonry/Controllers/TransportCategoryController.cs
+++ ecommerce-masonry/ecommerce-masonry/Controllers/TransportCategoryController.cs
@@ -37,14 +37,9 @@ namespace ecommerce_masonry.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(TransportCategory obj)
         {
-            if (ModelState.IsValid)
-            {
-                _db.TransportCategory.Add(obj);
-                _db.SaveChanges();
-
-                return RedirectToAction("Index"); // RedirectToAction redirects us to the index in this controler, so no need to define the name of the controller here.
-            }
-            return View(obj);
+            _db.TransportCategory.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index"); // RedirectToAction redirects us to the index in this controler, so no need to define the name of the controller here.
         }
     }
 }
--- ecommerce-masonry/ecommerce-masonry/Models/ApplicationType.cs
+++ ecommerce-masonry/ecommerce-masonry/Models/ApplicationType.cs
@@ -6,7 +6,6 @@ namespace ecommerce_masonry.Models
     {
         [Key] // Once we add this annotation it tells entity framework that this column needs to be an id column and pkey for our table
         public int ID { get; set; }
-        [Required]
         public string CategoryName { get; set; }
 
     }
