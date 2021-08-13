53ab2adb2f37d8d114aa4cfdf9a90e44c2ae9cd2
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-08-13 23:46:54 +0000

    New Product model added. No migration was done. Must watch migration tutorials since I've updated previous models with Required tags.

---
+++ ecommerce-masonry/ecommerce-masonry/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ecommerce_masonry.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Range(0.1, double.MaxValue)]
+        public double Price { get; set; }
+        public string Image { get; set; }
+
+        [Display(Name = "Category Type")]
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+    }
+}
