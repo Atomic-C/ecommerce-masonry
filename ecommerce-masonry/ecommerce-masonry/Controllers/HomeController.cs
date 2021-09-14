39aabf4091d4bf07e50bd05f4673425fa7f16c93
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-09-14 21:08:39 +0000

    We can now add items to our session, or Cart.

--- ecommerce-masonry/ecommerce-masonry/Controllers/HomeController.cs
+++ ecommerce-masonry/ecommerce-masonry/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using ecommerce_masonry.Data;
 using ecommerce_masonry.Models;
 using ecommerce_masonry.Models.ViewModels;
+using ecommerce_masonry.Utility;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -42,6 +44,21 @@ namespace ecommerce_masonry.Controllers
                 IsInCart = false
             };
             return View(DetailsViewModel);
+        }
+        
+        [HttpPost,ActionName("Details")]
+        public IActionResult DetailsPost(int id)
+        {
+
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WebConstance.SessionCart) !=null && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WebConstance.SessionCart).Count() > 0)
+            {
+                // Here we know session exists, we want to retrive that session and add an item to it
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WebConstance.SessionCart); // If there's something, it gts retrived 
+            }
+            shoppingCartList.Add(new ShoppingCart { ProductId = id }); // If empty we directly add to it
+            HttpContext.Session.Set(WebConstance.SessionCart, shoppingCartList);
+            return RedirectToAction(nameof(Index)); // To redirect back to home page we use redirect and nameof method, of an action method to avoid magic strings.
         }
 
         public IActionResult Privacy()
