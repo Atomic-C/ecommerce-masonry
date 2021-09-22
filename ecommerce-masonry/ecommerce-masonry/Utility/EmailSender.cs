fa3ba6e3dd8eba7fc6bc1ed330612d0cb2a00594
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-09-22 19:29:24 +0000

    Implmented Mailjet, configured it

---
+++ ecommerce-masonry/ecommerce-masonry/Utility/EmailSender.cs
@@ -0,0 +1,63 @@
+﻿using Mailjet.Client;
+using Mailjet.Client.Resources;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ecommerce_masonry.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            return Execute(email, subject, htmlMessage);
+        }
+
+        public async Task Execute(string email, string subject, string body)
+        {
+            MailjetClient client = new MailjetClient("d054ba6ee7ece3fc564ad628a74c34a2", "a176d8b3955b0ab4c66946dddf2b8566")
+            {
+                Version = ApiVersion.V3_1,
+            };
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = Send.Resource,
+            }
+             .Property(Send.Messages, new JArray {
+     new JObject {
+      {
+       "From",
+       new JObject {
+        {"Email", "pedro.p.sousa@sapo.pt"},
+        {"Name", "Pedro"}
+       }
+      }, {
+       "To",
+       new JArray {
+        new JObject {
+         {
+          "Email",
+          email
+         }, {
+          "Name",
+          "DotNetMastery"
+         }
+        }
+       }
+      }, {
+       "Subject",
+       subject
+      }, {
+       "HTMLPart",
+       body
+      }
+     }
+             });
+           // MailjetResponse response = await client.PostAsync(request);
+            await client.PostAsync(request);
+        }
+    }
+}
