0a938d438f4fc63fef4263baacd16e0382be999d
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-10-28 19:55:24 +0000

    Added InquiryDetails Model, so we can save the user inquiry in our database in case email gets lost.

--- ecommerce-masonry/Masonry_Models/InquiryDetails.cs
+++ ecommerce-masonry/Masonry_Models/InquiryDetails.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Masonry_Models
 {
-    class InquiryDetails
+    public class InquiryDetails
     {
+        [Key]
+        public int MyProperty { get; set; }
+
+        [Required]
+        public string InquiryHeaderId { get; set; } // We need a FK reference for our Inquiry Header
+        [ForeignKey("InquiryHeaderId")]
+        public InquiryHeader InquiryHeader { get; set; }
+
+        [Required]
+        public string ProductId { get; set; } // We need a FK reference for our Inquiry Header
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
     }
 }
