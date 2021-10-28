b78b60491023b0e4519154b840dd0df47c4c2f53
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-10-28 19:43:34 +0000

    Added InquiryHeader Model, so we can save user details from inquiry, in our database in case email gets lost.

---
+++ ecommerce-masonry/Masonry_Models/InquiryHeader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masonry_Models
+{
+    public class InquiryHeader
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]
+        public ApplicationUser Application { get; set; }
+        public DateTime InquiryDate { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        public string Email { get; set; }
+
+    }
+}
