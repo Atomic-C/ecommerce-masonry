45e1f66e16b2299d882f60fcde15730aaeb1452b
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-10-29 20:13:32 +0000

    Added InquiryHeader to the repository. Next step is adding InquiryDetails to repo!

---
+++ ecommerce-masonry/Masonry_Data_Access/Repository/InquiryHeaderRepository.cs
@@ -0,0 +1,31 @@
+﻿using Masonry_Data_Access.Repository.IRepository;
+using Masonry_Models;
+using Masonry_Utility;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masonry_Data_Access.Repository
+{
+    public class InquiryHeaderRepository : Repository<InquiryHeader>, IInquiryHeaderRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public InquiryHeaderRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        
+
+        public void Update(InquiryHeader obj)
+        {
+            _db.InquiryHeaders.Update(obj);
+        }
+
+
+    }
+}
