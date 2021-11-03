424d017c699532c4dc5a2f257d63b5a123388085
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-11-03 18:33:12 -0100

    Added InquiryViewModel and InquiryController

---
+++ ecommerce-masonry/ecommerce-masonry/Controllers/InquiryController.cs
@@ -0,0 +1,27 @@
+﻿using Masonry_Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Masonry_Utility;
+using Masonry_Data_Access;
+using Masonry_Data_Access.Repository.IRepository;
+
+namespace ecommerce_masonry.Controllers
+{
+    public class InquiryController : Controller
+    {
+    private readonly IInquiryHeaderRepository _inquiryHeaderRepo;
+    private readonly IInquiryDetailsRepository _inquiryDetailsRepo;
+
+        public InquiryController(IInquiryHeaderRepository inquiryHeaderRepository, IInquiryDetailsRepository inquiryDetailsRepository)
+        {
+            _inquiryHeaderRepo = inquiryHeaderRepository; // We add dependency injection for header and detail
+            _inquiryDetailsRepo = inquiryDetailsRepository;
+        }
+
+        public IActionResult Index()
+        {
+
+        }
+    }
+}
