25d8baeaa4f39b257f7a6f6bc6df49d5dc1c04c2
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-11-17 18:19:15 -0100

    Added validations to Summary, admins now see Submit Order instead of submit inquiry for users.

--- ecommerce-masonry/Masonry_Models/ApplicationUser.cs
+++ ecommerce-masonry/Masonry_Models/ApplicationUser.cs
@@ -10,14 +10,19 @@ namespace Masonry_Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Required]
         public string FullUserName { get; set; }
         // We use NotMapped because we don't want to add this to the database, and only use these properties in our view!
+        [Required]
         [NotMapped]
         public string   StreetAddress { get; set; }
+        [Required]
         [NotMapped]
         public string   City { get; set; }
+        [Required]
         [NotMapped]
         public string   State { get; set; }
+        [Required]
         [NotMapped]
         public string   PostalCode { get; set; }
     }
