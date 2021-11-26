05a33440778e7daffb67d9ba2c1398bf27cca13a
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-11-26 19:23:04 -0100

    Added new OrderController for admin orders, so admins can see their orders! Currently it's only available to see customer orders.

---
+++ ecommerce-masonry/ecommerce-masonry/Controllers/OrderController.cs
@@ -0,0 +1,39 @@
+﻿using Masonry_Data_Access.Repository.IRepository;
+using Masonry_Models.ViewModels;
+using Masonry_Utility.BrainTree;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ecommerce_masonry.Controllers
+{
+    public class OrderController : Controller
+    {
+        //private readonly ApplicationDbContext _db;
+
+        private readonly IOrderDetailRepository _orderDetailRepo;
+        private readonly IOrderHeaderRepository _orderHeaderRepo;
+        private readonly IBrainTreeGate _brain;
+
+
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly IEmailSender _emailSender;
+
+        public OrderController(IOrderDetailRepository orderDetailRepo, IOrderHeaderRepository orderHeaderRepo, IBrainTreeGate brain)
+        {
+            //_db = db;
+
+            _orderDetailRepo = orderDetailRepo;
+            _orderHeaderRepo = orderHeaderRepo;
+            _brain = brain;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
