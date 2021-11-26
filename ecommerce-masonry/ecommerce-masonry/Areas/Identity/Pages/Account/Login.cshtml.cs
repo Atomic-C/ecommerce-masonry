3b5b3c3ada23a2f1ae943ec503fe3d49b9fe1fee
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-11-26 18:18:30 -0100

    First stage of register account page UI redesign

--- ecommerce-masonry/ecommerce-masonry/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ ecommerce-masonry/ecommerce-masonry/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -100,11 +100,12 @@ namespace ecommerce_masonry.Areas.Identity.Pages.Account
                 }
                 else
                 {
+                    ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
                     ModelState.AddModelError(string.Empty, "Invalid login attempt.");
                     return Page();
                 }
             }
-
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             // If we got this far, something failed, redisplay form
             return Page();
         }
