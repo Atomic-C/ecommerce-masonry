7f854c60ac0901f986454b412d3715d8feba2092
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-12-07 20:17:27 -0100

    Empty commit. Just testing migrations.

---
+++ ecommerce-masonry/Masonry_Data_Access/Migrations/20211207204720_general-update-check.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Masonry_Data_Access.Migrations
+{
+    public partial class generalupdatecheck : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
